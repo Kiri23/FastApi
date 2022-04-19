060c4a9862967bf29bc9339379f3b951b74663a0
kiri23 <christian@opticpower.com>
2022-04-19 09:11:13 -0400

    add main.py file

---
+++ main.py
@@ -0,0 +1,19 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World 2"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
+# To allow clicking the play button in vscode and uvicorn command is run
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
