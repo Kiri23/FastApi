1e0b2f855d1f2dcde349e7264d71bcea3cd20f2a
kiri23 <christian@opticpower.com>
2022-08-29 07:53:49 -0400

    add comment to version.py to explain how to increase the version number

--- version.py
+++ version.py
@@ -2,13 +2,16 @@ from fastapi import FastAPI
 from fastapi.openapi.utils import get_openapi
 import json
 
+# To increase version use bumpversion --current-version 0.1.2 patch version.py
+# or this file to configuration file of bumpversion
+
 
 def custom_openapi(app: FastAPI):
     if app.openapi_schema:
         return app.openapi_schema
     openapi_schema = get_openapi(
         title="KIRI SDK",
-        version="0.1.2",
+        version="0.1.3",
         description="This is a very custom OpenAPI SDK",
         routes=app.routes,
     )
